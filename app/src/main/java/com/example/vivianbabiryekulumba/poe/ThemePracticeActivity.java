fb414204bb4a276fb480a03d017c51cbdbac3f73
ViviKulu <babimaji1111@gmail.com>
2018-07-20 23:43:06 -0400

    new navigation

--- app/src/main/java/com/example/vivianbabiryekulumba/poe/ThemePracticeActivity.java
+++ app/src/main/java/com/example/vivianbabiryekulumba/poe/ThemePracticeActivity.java
@@ -3,20 +3,17 @@ package com.example.vivianbabiryekulumba.poe;
 import android.animation.Animator;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.vivianbabiryekulumba.poe.network.PoeNetworkService;
 import com.example.vivianbabiryekulumba.poe.recyclerview.Poem;
 import com.example.vivianbabiryekulumba.poe.recyclerview.ThemeAdapter;
@@ -24,6 +21,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -39,10 +37,10 @@ public class ThemePracticeActivity extends AppCompatActivity {
     private static List<Poem> poemList;
     RecyclerView recyclerView;
     ThemeAdapter themeAdapter;
-    View fabLayout;
-    FloatingActionButton fab_base, fab1, fab2, fab3, fab4;
+    View menuBGLayout;
+    TextView menu_tab, tab1, tab2, tab3, tab4;
     LinearLayout ll_base, ll1, ll2, ll3, ll4;
-    boolean isFABOpen = false;
+    boolean isMenuOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,52 +48,54 @@ public class ThemePracticeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_theme_practice);
         recyclerView = findViewById(R.id.theme_recycler_practice);
 
-        fabLayout = findViewById(R.id.fabBGLayout);
-        ll_base = findViewById(R.id.fab_layout_base);
-        ll1 = findViewById(R.id.fab_layout_home);
-        ll2 = findViewById(R.id.fab_layout_my_work);
-        ll3 = findViewById(R.id.fab_layout_translate);
-        ll4 = findViewById(R.id.fab_layout_save_draft);
-        fab_base = findViewById(R.id.floating_button_base);
-        fab1 = findViewById(R.id.floating_button_home);
-        fab2 = findViewById(R.id.floating_button_my_work);
-        fab3 = findViewById(R.id.floating_button_translate);
-        fab4 = findViewById(R.id.floating_button_save_draft);
+        menuBGLayout = findViewById(R.id.menuBGLayout);
+        ll_base = findViewById(R.id.menu_layout_base);
+        ll1 = findViewById(R.id.tab_layout_1);
+        ll2 = findViewById(R.id.tab_layout_2);
+        ll3 = findViewById(R.id.tab_layout_3);
+        ll4 = findViewById(R.id.tab_layout_4);
+        menu_tab = findViewById(R.id.menu_tab);
+        tab1 = findViewById(R.id.tab1);
+        tab2 = findViewById(R.id.tab2);
+        tab3 = findViewById(R.id.tab3);
+        tab4 = findViewById(R.id.tab4);
 
-        fabLayout.setOnClickListener(new View.OnClickListener() {
+        menuBGLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                closeFABMenu();
+                closeTabMenu();
             }
         });
 
-        fab_base.setOnClickListener(new View.OnClickListener() {
+        hideTabs(tab1, tab2, tab3, tab4);
+
+        menu_tab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!isFABOpen){
-                    showFABMenu();
-                    fab1.setOnClickListener(new View.OnClickListener() {
+                if (!isMenuOpen) {
+                    showTabMenu();
+                    tab1.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             Intent intent = new Intent(ThemePracticeActivity.this, PoemActivity.class);
                             startActivity(intent);
                         }
                     });
-                    fab2.setOnClickListener(new View.OnClickListener() {
+                    tab2.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             Intent intent = new Intent(ThemePracticeActivity.this, MyWorkActivity.class);
                             startActivity(intent);
                         }
                     });
-                    fab3.setOnClickListener(new View.OnClickListener() {
+                    tab3.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             //Make retrofit to google translate api and translate current content from poet
                             //then with an intent take translated content to Card content activity.
                         }
                     });
-                    fab4.setOnClickListener(new View.OnClickListener() {
+                    tab4.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             //Write to the fb-db. index : my_work. Should save into database.
@@ -106,8 +106,8 @@ public class ThemePracticeActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(), "Saved draft to My Work!", Toast.LENGTH_SHORT).show();
                         }
                     });
-                }else{
-                    closeFABMenu();
+                } else {
+                    closeTabMenu();
                 }
             }
         });
@@ -115,6 +115,7 @@ public class ThemePracticeActivity extends AppCompatActivity {
         getRetrofit();
         Toast.makeText(getApplicationContext(), "Complete the thought", Toast.LENGTH_SHORT).show();
     }
+
 
     public void getRetrofit() {
         retrofit = new Retrofit.Builder()
@@ -145,26 +146,37 @@ public class ThemePracticeActivity extends AppCompatActivity {
         });
     }
 
-    private void showFABMenu(){
-        isFABOpen= true;
+    private void hideTabs(TextView tab1, TextView tab2, TextView tab3, TextView tab4) {
+        if (!isMenuOpen) {
+            tab1.setVisibility(View.INVISIBLE);
+            tab2.setVisibility(View.INVISIBLE);
+            tab3.setVisibility(View.INVISIBLE);
+            tab4.setVisibility(View.INVISIBLE);
+        }else{
+            showTabMenu();
+        }
+    }
+
+    private void showTabMenu() {
+        isMenuOpen = true;
         ll_base.setVisibility(View.VISIBLE);
         ll1.setVisibility(View.VISIBLE);
         ll2.setVisibility(View.VISIBLE);
         ll3.setVisibility(View.VISIBLE);
         ll4.setVisibility(View.VISIBLE);
         ll_base.setVisibility(View.VISIBLE);
-        fabLayout.setVisibility(View.VISIBLE);
+        menuBGLayout.setVisibility(View.VISIBLE);
 
         ll_base.animate().rotationBy(360);
-        ll1.animate().translationY(-getResources().getDimension(R.dimen.standard_75));
-        ll2.animate().translationY(-getResources().getDimension(R.dimen.standard_150));
-        ll3.animate().translationY(-getResources().getDimension(R.dimen.standard_225));
-        ll4.animate().translationY(-getResources().getDimension(R.dimen.standard_300));
+        ll1.animate().translationY(-getResources().getDimension(R.dimen.standard_45));
+        ll2.animate().translationY(-getResources().getDimension(R.dimen.standard_65));
+        ll3.animate().translationY(-getResources().getDimension(R.dimen.standard_85));
+        ll4.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
     }
 
-    private void closeFABMenu(){
-        isFABOpen=false;
-        fabLayout.setVisibility(View.GONE);
+    private void closeTabMenu() {
+        isMenuOpen = false;
+        menuBGLayout.setVisibility(View.GONE);
         ll_base.animate().rotationBy(-360);
         ll1.animate().translationY(2);
         ll2.animate().translationY(2);
@@ -172,12 +184,15 @@ public class ThemePracticeActivity extends AppCompatActivity {
         ll4.animate().translationY(2).setListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
-
+                tab1.setVisibility(View.VISIBLE);
+                tab2.setVisibility(View.VISIBLE);
+                tab3.setVisibility(View.VISIBLE);
+                tab4.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onAnimationEnd(Animator animator) {
-                if(!isFABOpen){
+                if (!isMenuOpen) {
                     ll1.setVisibility(View.GONE);
                     ll2.setVisibility(View.GONE);
                     ll3.setVisibility(View.GONE);
@@ -200,9 +215,9 @@ public class ThemePracticeActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if(isFABOpen){
-            closeFABMenu();
-        }else{
+        if (isMenuOpen) {
+            closeTabMenu();
+        } else {
             super.onBackPressed();
         }
     }
